ec8b6147b5c4b4e290bc8dc1807ecde742ce2b07
Kemer <kitowski.przemek@gmail.com>
2018-11-04 16:54:52 +0100

    Comments for .js file

--- file.js
+++ file.js
@@ -1,13 +1,13 @@
 function printBox(width, height, style = '# ', left = 0, top = 0){
-  for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
+  for (let i = 0; i < height; i++) {       //1 - Ln 2 Col 3 for (let i = 0; i < ... - For statement
+    for (let j = 0; j < width; j++) {      //1 - Ln 3 Col 5 for (let j = 0; j < ... - For statement
       result += style
     }
-    if (i !== height - 1) {
+    if (i !== height - 1) {                //2 - Ln 6 Col 5 if (i !== height -1 - If with else statement
       result += EOL + curForward(left)
     } else {
       result += EOL
     }
   }
-  return result
+  return result                            //1 - Ln 12 Col 3 return result - Return statement
 }
